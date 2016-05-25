59d0bb989eb95c147278e00c10413ddebc369701
sMortens <spencer.mortenson@gmail.com>
2016-05-25 04:27:43 +0000

    Fixed nav on scroll

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,38 @@
+
 $(document).on('page:load',function() {
+   $('.fadeIn').hide().delay(300).fadeIn(2000,function(){
+
+
+
+  });
+	   $(window).bind('scroll', function() {
+     var h = $('nav').height();
+	   var navHeight = $( window ).height()-h*3;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
+
+});
+$(document).ready(function() {
   $('.fadeIn').hide().delay(300).fadeIn(2000,function(){
 
 
 
   });
-
+	   $(window).bind('scroll', function() {
+     var h = $('nav').height();
+	   var navHeight = $( window ).height()-h*3;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
 
 });
-
 
