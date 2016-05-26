f9b18e8668d319ed01407cc47df46068bf202e4c
sMortens <spencer.mortenson@gmail.com>
2016-05-26 00:47:32 +0000

    removed nav lock

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,17 +5,17 @@ $(document).on('page:load',function() {
 
 
   });
-	   $(window).bind('scroll', function() {
+	  /* $(window).bind('scroll', function() {
      var h = $('nav').height();
 	   var navHeight = $( window ).height()-h;
-     
+
 			 if ($(window).scrollTop() > navHeight) {
 				 $('nav').addClass('fixed');
 			 }
 			 else {
 				 $('nav').removeClass('fixed');
 			 }
-		});
+		});*/
 
 });
 $(document).ready(function() {
@@ -24,17 +24,17 @@ $(document).ready(function() {
 
 
   });
-	   $(window).bind('scroll', function() {
+	  /* $(window).bind('scroll', function() {
      var h = $('nav').height();
 	  var navHeight = $( window ).height()-h;
-       
+
 			 if ($(window).scrollTop() > navHeight) {
 				 $('nav').addClass('fixed');
 			 }
 			 else {
 				 $('nav').removeClass('fixed');
 			 }
-		});
+		});*/
 
 });
 
